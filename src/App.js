1848cb9c07e182ced9781fa294f07ce9dfdb7c48
rohini-zingworks <91865056+rohini-zingworks@users.noreply.github.com>
2021-11-16 12:50:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Rohini Nagtilak</h1>
+      <p>Learn CI/CD for react</p>
+    </div>
+  );
+}
+
+export default App;
